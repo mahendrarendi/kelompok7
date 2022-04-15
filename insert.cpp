970a8dd33904c54cadbc008297041e2a12824dd9
Rendi Nicolas Mahendra <68139718+mahendrarendi@users.noreply.github.com>
2022-04-15 22:21:54 +0700

    insert sorting

---
+++ insert.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+//p
+int main(){
+    //input nilai
+	int y;
+	cout<<"Masukan Banyak Bilangan:";
+	cin>>y;
+	//proses sorting
+	int x[y];
+	for(int i = 1; i<=y; i++){
+		cout<<"Masukan Bilangan ke - "<<i<<" :";
+		cin>>x[i];
+		cout<<endl;
+	}
+	//
+	for(int i=1; i<y; i++){
+		int key = x[i];
+		int j = i-1;
+		while(j>=1 && x[j] > key){
+			x[j+1] = x[j];
+			j--;
+		}
+		x[j+1] = key;
+		cout<<"proses sorting"<<endl;
+		for(int m=1;m<=y;m++){
+		cout<<x[m]<<" ";
+	    }
+	    cout<<endl;
+	}
+	//output
+	cout<<"hasil sorting"<<endl;
+	for(int m=1;m<=y;m++){
+		cout<<x[m]<<" ";
+	}
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<"develop @kelompok_7 ";
+}
